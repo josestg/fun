1580ad5dc752d5bb2e31d48d2e3d7a14b27fd6d8
Jose Alfredo Sitanggang <josealfredositanggang@gmail.com>
2024-02-20 16:32:51 +0700

    feat: impl useful function for working with slice

---
+++ slices/slices.go
@@ -0,0 +1,77 @@
+package slices
+
+import "github.com/josestg/fun/cmp"
+
+// Some returns true if at least one element in the slice satisfies the predicate.
+func Some[T any](s []T, p cmp.Predicate[T]) bool {
+	for _, v := range s {
+		if p(v) {
+			return true
+		}
+	}
+	return false
+}
+
+// All returns true if all elements in the slice satisfy the predicate.
+func All[T any](s []T, p cmp.Predicate[T]) bool {
+	for _, v := range s {
+		if !p(v) {
+			return false
+		}
+	}
+	return true
+}
+
+// Filter returns a new slice containing only the elements that satisfy the predicate.
+func Filter[T any](s []T, p cmp.Predicate[T]) []T {
+	n := Count(s, p)
+	return Fold(
+		s,
+		make([]T, 0, n),
+		func(xs []T, x T) []T {
+			if p(x) {
+				xs = append(xs, x)
+			}
+			return xs
+		},
+	)
+}
+
+// Count returns the number of elements in the slice that satisfy the predicate.
+func Count[T any](s []T, p cmp.Predicate[T]) int {
+	return Fold(s, 0, func(n int, x T) int {
+		if p(x) {
+			n++
+		}
+		return n
+	})
+}
+
+// Map returns a new slice containing the result of applying the function to each element in the slice.
+func Map[T, U any](s []T, f func(T) U) []U {
+	return Fold(
+		s,
+		make([]U, 0, len(s)),
+		func(xs []U, x T) []U { return append(xs, f(x)) },
+	)
+}
+
+// Find returns the first element in the slice that satisfies the predicate.
+func Find[T any](s []T, p cmp.Predicate[T]) (T, bool) {
+	for _, v := range s {
+		if p(v) {
+			return v, true
+		}
+	}
+	var zero T
+	return zero, false
+}
+
+// Fold applies the function to each element in the slice, accumulating the result.
+func Fold[T, U any](s []T, z U, f func(U, T) U) U {
+	r := z
+	for _, v := range s {
+		r = f(r, v)
+	}
+	return r
+}
---
+++ slices/slices_test.go
@@ -0,0 +1,96 @@
+package slices
+
+import (
+	"github.com/josestg/fun/cmp"
+	"testing"
+)
+
+type Task struct {
+	ID        int
+	Title     string
+	Completed bool
+}
+
+func Completed(t Task) bool { return t.Completed }
+
+func Complete(t Task) Task {
+	t.Completed = true
+	return t
+}
+
+var tasks = []Task{
+	{1, "one", true},
+	{2, "two", false},
+	{3, "three", true},
+}
+
+func TestSome(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	if !Some(s, cmp.Eq(3)) {
+		t.Error("expect found some value that equal to 3")
+	}
+
+	if Some(s, cmp.Gt(5)) {
+		t.Error("expect no value that greater than 5")
+	}
+
+	ok := Some(tasks, Completed)
+	if !ok {
+		t.Error("expect some task is completed")
+	}
+}
+
+func TestAll(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	if !All(s, cmp.Gte(1)) {
+		t.Error("expect all values greater or equal to 1")
+	}
+
+	if All(s, cmp.Gt(1)) {
+		t.Error("not all values is greater or equal to 1")
+	}
+
+	ok := All(tasks, Completed)
+	if ok {
+		t.Error("some task are not completed")
+	}
+}
+
+func TestFilter(t *testing.T) {
+	completedTasks := Filter(tasks, Completed)
+	ok := All(completedTasks, Completed)
+	if !ok {
+		t.Error("expect all task are completed")
+	}
+}
+
+func TestMap(t *testing.T) {
+	ok := All(Map(tasks, Complete), Completed)
+	if !ok {
+		t.Error("expect all task are completed")
+	}
+}
+
+func TestCount(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	n := Count(s, cmp.Lt(3))
+	if n != 2 {
+		t.Error("expect there are 2 items < 3")
+	}
+}
+
+func TestFind(t *testing.T) {
+	s := []int{1, 2, 3, 4, 5}
+	v, ok := Find(s, cmp.Gt(3))
+	if !ok {
+		t.Error("expect found value > 3")
+	}
+	if v != 4 {
+		t.Errorf("expect found value 4, got %d", v)
+	}
+
+	v, ok = Find(s, cmp.Gt(5))
+	if ok {
+		t.Error("expect not found value > 5")
+	}
+}
