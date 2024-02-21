619700ee3d9255064cf48a1a07528e6fc7594c4e
Jose Alfredo Sitanggang <josealfredositanggang@gmail.com>
2024-02-21 12:25:16 +0700

    fix: type inference on slice to make it working on any type that based on E.
    ref:
    - https://go.dev/ref/spec#Type_inference
    - https://go.dev/blog/intro-generics#Constraint_type_inference

--- slices/slices.go
+++ slices/slices.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Some returns true if at least one element in the slice satisfies the predicate.
-func Some[T any](s []T, p cmp.Predicate[T]) bool {
+func Some[S ~[]E, E any](s S, p cmp.Predicate[E]) bool {
 	for _, v := range s {
 		if p(v) {
 			return true
@@ -15,7 +15,7 @@ func Some[T any](s []T, p cmp.Predicate[T]) bool {
 }
 
 // All returns true if all elements in the slice satisfy the predicate.
-func All[T any](s []T, p cmp.Predicate[T]) bool {
+func All[S ~[]E, E any](s S, p cmp.Predicate[E]) bool {
 	for _, v := range s {
 		if !p(v) {
 			return false
@@ -25,12 +25,12 @@ func All[T any](s []T, p cmp.Predicate[T]) bool {
 }
 
 // Filter returns a new slice containing only the elements that satisfy the predicate.
-func Filter[T any](s []T, p cmp.Predicate[T]) []T {
+func Filter[S ~[]E, E any](s S, p cmp.Predicate[E]) S {
 	n := Count(s, p)
 	return Fold(
 		s,
-		make([]T, 0, n),
-		func(xs []T, x T) []T {
+		make(S, 0, n),
+		func(xs S, x E) S {
 			if p(x) {
 				xs = append(xs, x)
 			}
@@ -40,8 +40,8 @@ func Filter[T any](s []T, p cmp.Predicate[T]) []T {
 }
 
 // Count returns the number of elements in the slice that satisfy the predicate.
-func Count[T any](s []T, p cmp.Predicate[T]) int {
-	return Fold(s, 0, func(n int, x T) int {
+func Count[S ~[]E, E any](s S, p cmp.Predicate[E]) int {
+	return Fold(s, 0, func(n int, x E) int {
 		if p(x) {
 			n++
 		}
@@ -50,27 +50,27 @@ func Count[T any](s []T, p cmp.Predicate[T]) int {
 }
 
 // Map returns a new slice containing the result of applying the function to each element in the slice.
-func Map[T, U any](s []T, f func(T) U) []U {
+func Map[SE ~[]E, ST []T, E, T any](s SE, f func(E) T) ST {
 	return Fold(
 		s,
-		make([]U, 0, len(s)),
-		func(xs []U, x T) []U { return append(xs, f(x)) },
+		make(ST, 0, len(s)),
+		func(xs ST, x E) ST { return append(xs, f(x)) },
 	)
 }
 
 // Find returns the first element in the slice that satisfies the predicate.
-func Find[T any](s []T, p cmp.Predicate[T]) (T, bool) {
+func Find[S ~[]E, E any](s S, p cmp.Predicate[E]) (E, bool) {
 	for _, v := range s {
 		if p(v) {
 			return v, true
 		}
 	}
-	var zero T
+	var zero E
 	return zero, false
 }
 
 // Fold applies the function to each element in the slice, accumulating the result.
-func Fold[T, U any](s []T, z U, f func(U, T) U) U {
+func Fold[S ~[]E, E, T any](s S, z T, f func(T, E) T) T {
 	r := z
 	for _, v := range s {
 		r = f(r, v)
@@ -79,7 +79,7 @@ func Fold[T, U any](s []T, z U, f func(U, T) U) U {
 }
 
 // Each applies the function to each element in the slice.
-func Each[T any](s []T, f func(T)) {
+func Each[S ~[]E, E any](s S, f func(E)) {
 	for _, v := range s {
 		f(v)
 	}
